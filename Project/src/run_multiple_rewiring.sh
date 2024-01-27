985d543dd54fb7c390dbb58193627ada3cbd5d78
lucia-pezzetti <131250932+lucia-pezzetti@users.noreply.github.com>
2024-01-27 09:14:44 +0100

    Delete Project/src directory

--- Project/src/run_multiple_rewiring.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ARGS=(10 20 30 40 50 60 70 80 90)
-
-# Loop through the arguments and run the program
-for i in "${ARGS[@]}"
-do
-    python main.py "1000" "../data/Rewiring_overlap/list_multiplex_0.$i.txt"
-done
