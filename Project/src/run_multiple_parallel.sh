6fd762223eb9d550c8bdb0dec71e09c78fa02904
lucia-pezzetti <131250932+lucia-pezzetti@users.noreply.github.com>
2024-01-27 20:15:07 +0100

    Add files via upload

---
+++ Project/src/run_multiple_parallel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OVERLAPS=(0 0.20 0.40)
+LISTS=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+ITERATIONS=(0 1 2 3 4 5)
+MAX_PARALLEL_JOBS=10  # Adjust the number of parallel jobs
+
+# Function to run the command
+run_command() {
+    overlap=$1
+    list=$2
+    iteration=$3
+    python main.py "1000" "../data/multiplex_overlap_$overlap/list_multiplex_$list.txt" "../output_files/output_file_katz_$overlap/list_$list/iteration_$iteration.txt"
+}
+export -f run_command
+
+# Generate and run the commands in parallel
+parallel -j $MAX_PARALLEL_JOBS run_command ::: "${OVERLAPS[@]}" ::: "${LISTS[@]}" ::: "${ITERATIONS[@]}"
---
+++ Project/src/run_real_duplex.sh
@@ -0,0 +1,37 @@
+python main.py "6981" "../data/real_world_duplex/Arabidopsis_Duplex_L_1_2/list_multiplex.txt" "../output_files/real_world_duplex/Arabidopsis_Duplex_L_1_2/output_file.txt"
+
+python main.py "14490" "../data/real_world_duplex/arXiv_Duplex_L_2_6/list_multiplex.txt" "../output_files/real_world_duplex/arXiv_Duplex_L_2_6/output_file.txt"
+
+python main.py "14490" "../data/real_world_duplex/arXiv_Duplex_L_3_6/list_multiplex.txt" "../output_files/real_world_duplex/arXiv_Duplex_L_3_6/output_file.txt"
+
+python main.py "62" "../data/real_world_duplex/CS-Aarhus_Duplex_L_1_5/list_multiplex.txt" "../output_files/real_world_duplex/CS-Aarhus_Duplex_L_1_5/output_file.txt"
+
+python main.py "8216" "../data/real_world_duplex/Drosophila_Duplex_L_1_2/list_multiplex.txt" "../output_files/real_world_duplex/Drosophila_Duplex_L_1_2/output_file.txt"
+
+python main.py "8216" "../data/real_world_duplex/Drosophila_Duplex_L_1_3/list_multiplex.txt" "../output_files/real_world_duplex/Drosophila_Duplex_L_1_3/output_file.txt"
+
+python main.py "8216" "../data/real_world_duplex/Drosophila_Duplex_L_1_4/list_multiplex.txt" "../output_files/real_world_duplex/Drosophila_Duplex_L_1_4/output_file.txt"
+
+python main.py "8216" "../data/real_world_duplex/Drosophila_Duplex_L_2_3/list_multiplex.txt" "../output_files/real_world_duplex/Drosophila_Duplex_L_2_3/output_file.txt"
+
+python main.py "215" "../data/real_world_duplex/FAO_Duplex_L_3_24/list_multiplex.txt" "../output_files/real_world_duplex/FAO_Duplex_L_3_24/output_file.txt"
+
+python main.py "18223" "../data/real_world_duplex/Homo_Duplex_L_1_2/list_multiplex.txt" "../output_files/real_world_duplex/Homo_Duplex_L_1_2/output_file.txt"
+
+python main.py "18223" "../data/real_world_duplex/Homo_Duplex_L_1_5/list_multiplex.txt" "../output_files/real_world_duplex/Homo_Duplex_L_1_5/output_file.txt"
+
+python main.py "18223" "../data/real_world_duplex/Homo_Duplex_L_2_5/list_multiplex.txt" "../output_files/real_world_duplex/Homo_Duplex_L_2_5/output_file.txt"
+
+python main.py "1006" "../data/real_world_duplex/HUM_HIV_Duplex_L_1_2/list_multiplex.txt" "../output_files/real_world_duplex/HUM_HIV_Duplex_L_1_2/output_file.txt"
+
+python main.py "7748" "../data/real_world_duplex/Mus_Duplex_L_1_3/list_multiplex.txt" "../output_files/real_world_duplex/Mus_Duplex_L_1_3/output_file.txt"
+
+python main.py "6571" "../data/real_world_duplex/SacchCere_Duplex_L_1_2/list_multiplex.txt" "../output_files/real_world_duplex/SacchCere_Duplex_L_1_2/output_file.txt"
+
+python main.py "6571" "../data/real_world_duplex/SacchCere_Duplex_L_1_7/list_multiplex.txt" "../output_files/real_world_duplex/SacchCere_Duplex_L_1_7/output_file.txt"
+
+python main.py "4093" "../data/real_world_duplex/SacchPomb_Duplex_L_3_4/list_multiplex.txt" "../output_files/real_world_duplex/SacchPomb_Duplex_L_3_4/output_file.txt"
+
+python main.py "4093" "../data/real_world_duplex/SacchPomb_Duplex_L_3_6/list_multiplex.txt" "../output_files/real_world_duplex/SacchPomb_Duplex_L_3_6/output_file.txt"
+
+python main.py "4093" "../data/real_world_duplex/SacchPomb_Duplex_L_4_6/list_multiplex.txt" "../output_files/real_world_duplex/SacchPomb_Duplex_L_4_6/output_file.txt"
